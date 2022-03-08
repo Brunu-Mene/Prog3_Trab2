9b3d07c61f0cd0ab6980391beffa898c7451a8dc
Brunu-Mene <bruno.ufes@hotmail.com>
2022-03-07 22:17:14 -0300

    finalizando

--- src/Compara.cpp
+++ src/Compara.cpp
@@ -2,46 +2,33 @@
 
 bool compara_candidato::operator ()(const Candidatos &c1, const Candidatos &c2){
     if(c1.getVotos_Nominais() == c2.getVotos_Nominais()){
-        if(c2.getdata_nasc() > c1.getdata_nasc()) return true;
-        return false;
+        return c1.getdata_nasc() < c2.getdata_nasc();
+
     }
-    if(c2.getVotos_Nominais() > c1.getVotos_Nominais()) return false;
-    return true;
+    return c1.getVotos_Nominais() > c2.getVotos_Nominais();
 }
 
 bool compara_vn_np::operator()(const Candidatos &c1, const Candidatos &c2){
     if(c1.getVotos_Nominais() == c2.getVotos_Nominais()){
-        if(c1.getNumero_Partido() > c2.getNumero_Partido()) return false;
-        return true;
+        return c2.getNumero_Partido() > c1.getNumero_Partido();
     }
-    if(c2.getVotos_Nominais() > c1.getVotos_Nominais()) return false;
-    return true;
+    return c1.getVotos_Nominais() > c2.getVotos_Nominais();
 }
 
 bool compara_vt_np::operator()(const Partidos &p1, const Partidos &p2){
     if(p1.getVotosTotal() == p2.getVotosTotal()){
-            if(p1.getNumero() > p2.getNumero()) 
-                return false;
-            return true;
-        }
-        if(p2.getVotosTotal() > p1.getVotosTotal()) 
-            return false;
-        return true;
+        return p2.getNumero() > p1.getNumero();
+    }
+    return p1.getVotosTotal() > p2.getVotosTotal();
 }
     
 
 bool compara_vt_vl_np::operator ()(const Partidos &p1, const Partidos &p2){
     if(p1.getVotosLegenda() == p2.getVotosLegenda()){
             if(p1.getVotosTotal() - p1.getVotosLegenda() == p2.getVotosTotal() - p2.getVotosLegenda()){
-                if(p1.getNumero() > p2.getNumero()) 
-                    return false;
-                return true;
+                return p2.getNumero() > p1.getNumero();
             }
-            if((p2.getVotosTotal() - p2.getVotosLegenda()) > (p1.getVotosTotal() - p1.getVotosLegenda()))
-                return false;
-            return true;
+            return (p1.getVotosTotal() - p1.getVotosLegenda()) > (p2.getVotosTotal() - p2.getVotosLegenda());
         }
-    if(p2.getVotosLegenda() > p1.getVotosLegenda())
-        return false;
-    return true;
+    return p1.getVotosLegenda() > p2.getVotosLegenda();
 }
